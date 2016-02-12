d6d033b6ca4dbfe7c4d6f9b3b5407bd5bdd2bc88
Kayvan Mazaheri <KayvanMazaheri@GMail.com>
2016-02-12 21:51:18 +0330

    rtl

--- js/main.js
+++ js/main.js
@@ -218,7 +218,7 @@ $(document).ready(function(){
     /* 4. Newsletter ........................ */
     /* ------------------------------------- */
 
-    // $("#notifyMe").notifyMe();
+     // $("#notifyMe").notifyMe();
 
     // (function() {
 
